498006fa473323d6bacc1308ebaae7def1080dc1
CoderDream <coderdream@gmail.com>
2019-04-19 00:44:20 +0800

    update

--- Chapter04/040203/AutoLayoutDemo/AutoLayoutDemo/ViewController.swift
+++ Chapter04/040203/AutoLayoutDemo/AutoLayoutDemo/ViewController.swift
@@ -13,6 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        let left = NSLayoutConstraint.init(item: <#T##Any#>,
+//                                           attribute: <#T##NSLayoutConstraint.Attribute#>,
+//                                           relatedBy: <#T##NSLayoutConstraint.Relation#>,
+//                                           toItem: <#T##Any?#>,
+//                                           attribute: <#T##NSLayoutConstraint.Attribute#>,
+//                                           multiplier: <#T##CGFloat#>,
+//                                           constant: <#T##CGFloat#>)
+        
+        _ = NSLayoutConstraint.Attribute.left
+        _ = NSLayoutConstraint.Relation.equal
+        let aSubView = (Any).self
+        let aSuperView = (Any).self
+        _ = NSLayoutConstraint.init(item: aSubView, attribute: .left, relatedBy: .equal, toItem: aSuperView, attribute: .left, multiplier: 1.0, constant: 100)
     }
 
 
